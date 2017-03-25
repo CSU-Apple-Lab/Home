e408208c65c4806dc8a88cef86b905862834d668
Equim <sayaka@ekyu.moe>
2017-03-26 02:18:42 +0800

    添加pm2配置文件；添加config样板

---
+++ init/config.example.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const configure = function () {
+    let port, host, mongo;
+
+    switch (process.env.NODE_ENV) {
+        case 'production':
+            host = '**';
+            port = 3000;
+            mongo = {
+                host: '**',
+                port: '**',
+                db: '**',
+                user: '**',
+                pass: '**'
+            };
+            break;
+
+        case 'development':
+        default:
+            host = 'localhost';
+            port = 2333;
+            mongo = {
+                host: 'localhost',
+                port: '**',
+                db: '**',
+                user: '**',
+                pass: '**'
+            };
+            break;
+    }
+
+    const config = {
+        system: {
+            host: host || '0.0.0.0',
+            port: process.env.PORT || port,
+        },
+        mongo: mongo
+    };
+
+    return config;
+}();
+
+module.exports = configure;
+global.config = configure;
