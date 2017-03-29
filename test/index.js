3d33758862e906b08f016092ba4119cf61e35cb5
Equim <sayaka@ekyu.moe>
2017-03-29 17:29:34 +0800

    v0.1.2 * 增加了测试单元 * 更新了依赖

---
+++ test/index.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const supertest = require('supertest');
+const chai = require('chai');
+const chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+chai.should();
+
+describe('Config Files', () => {
+    const mongoose = require('mongoose');
+
+    const initConfigPath = path.resolve(__dirname, '../init/config.js');
+    const initMongoosePath = path.resolve(__dirname, '../init/mongoose.js');
+
+    it('should have config files', () => {
+        fs.accessSync(initConfigPath);
+        fs.accessSync(initMongoosePath);
+    });
+
+    it('should have valid config files', () => {
+        const config = require(initConfigPath).init();
+
+        config.should.have.property('system');
+        config.system.host.should.not.be.empty;
+        config.system.port.should.be.a('number');
+
+        config.should.have.property('mongo');
+        global.config = config;
+        return require(initMongoosePath).init().should.be.fulfilled;
+    });
+
+    after((done) => {
+        delete global.config;
+        mongoose.connection.close(done);
+    });
+});
+
+describe('Services', () => {
+    let request;
+    before(() => {
+        const app = require('..');
+        request = supertest(app.listen());
+    });
+
+    describe('Static Resources', () => {
+        it('should respond with code 200 when the file exsists', (done) => {
+            request
+                .get('/member.html')
+                .expect(200, done);
+        });
+
+        it('should have a favicon', (done) => {
+            request
+                .get('/favicon.ico')
+                .expect(200, done);
+        });
+
+        it('should respond with an etag header', (done) => {
+            request
+                .get('/')
+                .expect(200)
+                .expect('etag', /^.+$/, done);
+        });
+    });
+
+    describe('APIs', () => {
+        it('should respond with code 200 when the request is valid');
+        it('should respond with code 400 when the request itself is malformed');
+        it('should respond with code 400 when the same record already exsists');
+    });
+});
